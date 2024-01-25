3a67cf8fa077335a0375ab32fb9c0d25640f57b0
Laela2012 <cute39zlela@yahoo.co.id>
2024-01-25 13:18:03 +0700

    questions and answers ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './app.css'
 import { useState } from 'react';
+import Trivia from './components/Trivia';
 
 function App() {
 
@@ -24,7 +25,14 @@ function App() {
 
   return (
     <div className="app">
-      <div className="main">main</div>
+      <div className="main">
+        <div className="top">
+          <div className="timer">30</div>
+        </div>
+        <div className="bottom">
+         <Trivia />
+        </div>
+      </div>
       <div className="pyramid">
         <ul className="moneyList">
           {moneyPyramid.map((m) => (
---
+++ src/components/Trivia.jsx
@@ -0,0 +1,13 @@
+export default function Trivia() {
+  return (
+    <div className="trivia">
+        <div className="question">What's the best youtube channel?</div>
+        <div className="answers">
+            <div className="answer wrong">Lama Dev</div>
+            <div className="answer">Almeera Channel</div>
+            <div className="answer">Lama Dev</div>
+            <div className="answer">Lama Dev</div>
+        </div>
+    </div>
+  )
+}
